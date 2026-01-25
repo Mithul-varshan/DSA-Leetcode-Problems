91c5f5fbe5ef01c5b33709d8fb1b81fb92983109
Mithul Varshan S K <mithul0605@gmail.com>
2026-01-25 17:49:57 +0530

    Time: 5 ms (85.43%) | Memory: 45.6 MB (69.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -18,17 +18,34 @@ class Solution {
         // }
         // return maxLen;
         
+        //BRUTE FORCE
+        // int maxLen =0;
+        // for(int i=0;i<s.length();i++){
+        //     int[] n = new int[256] ;
+        //     int len =0;
+        //     for(int j=i;j<s.length();j++){
+        //         if(n[s.charAt(j)] == 1) break;
+        //         len = j-i+1;
+        //         maxLen = Math.max(maxLen,len);
+        //         n[s.charAt(j)] = 1;
+        //     }
+        // }
+        // return maxLen;    
+
+        //Sliding window
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        int left = 0;
+        int n = s.length();
         int maxLen =0;
-        for(int i=0;i<s.length();i++){
-            int[] n = new int[256] ;
-            int len =0;
-            for(int j=i;j<s.length();j++){
-                if(n[s.charAt(j)] == 1) break;
-                len = j-i+1;
-                maxLen = Math.max(maxLen,len);
-                n[s.charAt(j)] = 1;
+        for(int right=0;right<n;right++){
+            char ch = s.charAt(right);
+            if(map.containsKey(ch) && map.get(ch) >= left){
+                left = map.get(ch) + 1;
             }
+            map.put(ch,right);
+            maxLen = Math.max(maxLen,right-left+1);
         }
-        return maxLen;    
+        return maxLen;
     }
 }
