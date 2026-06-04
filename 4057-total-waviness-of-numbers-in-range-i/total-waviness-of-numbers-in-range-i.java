7830b1af2be9e595a5940121cb97866c303420df
Mithul Varshan S K <mithul0605@gmail.com>
2026-06-04 10:11:48 -0700

    Time: 24 ms (82.90%) | Memory: 46.4 MB (67.29%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,36 +1,18 @@
-// class Solution {
-//     public int totalWaviness(int num1, int num2) {
-//         int wav = 0;
-//         for(int i=num1;i<=num2;i++){
-//             String s = String.valueOf(i);
-//             for(int j=1;j<s.length()-1;j++){
-//                 int n = (int)s.charAt(j);
-//                 int nPrev = (int)s.charAt(j-1);
-//                 int nNext = (int)s.charAt(j+1);
-//                 if((n > nPrev && n> nNext) || (n < nPrev && n < nNext)){
-//                     wav++;
-//                 }
-//             }
-//         }
-//         return wav;
-//     }
-// }
-
 class Solution {
     public int totalWaviness(int num1, int num2) {
         int wav = 0;
-
-        for (int i = num1; i <= num2; i++) {
-            char[] digits = Integer.toString(i).toCharArray();
-
-            for (int j = 1; j < digits.length - 1; j++) {
-                if ((digits[j] > digits[j - 1] && digits[j] > digits[j + 1]) ||
-                    (digits[j] < digits[j - 1] && digits[j] < digits[j + 1])) {
+        for(int i=num1;i<=num2;i++){
+            String s = String.valueOf(i);
+            for(int j=1;j<s.length()-1;j++){
+                int n = (int)s.charAt(j);
+                int nPrev = (int)s.charAt(j-1);
+                int nNext = (int)s.charAt(j+1);
+                if((n > nPrev && n> nNext) || (n < nPrev && n < nNext)){
                     wav++;
                 }
             }
         }
-
         return wav;
     }
 }
+
