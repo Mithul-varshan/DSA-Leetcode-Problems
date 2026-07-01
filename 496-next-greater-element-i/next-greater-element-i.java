c7f839b42c2e510acc438da7f7a571af65eae7e6
Mithul Varshan S K <mithul0605@gmail.com>
2026-07-01 20:29:08 +0530

    Time: 4 ms (74.85%) | Memory: 45.1 MB (61.75%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -16,18 +16,21 @@ class Solution {
         //     nums1[i] = ans; 
         // }
         // return nums1;
-        HashMap <Integer , Integer> hm = new HashMap<>();
-        Stack <Integer> s = new Stack();
-        int [] res = new int[nums1.length];
-        for (int ele : nums2){
-            while (!s.isEmpty() && s.peek()< ele){
-                hm.put(s.pop(),ele);
+
+        // Stack solution :
+        HashMap <Integer , Integer> map = new HashMap<>();
+        Stack <Integer> stack = new Stack();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
+                stack.pop();
             }
-            s.push (ele);
-            
-            for (int i=0 ; i < nums1.length ; i++){
-                res[i] = hm.containsKey(nums1[i])?hm.get(nums1[i]):-1;
-            }
+            if(stack.isEmpty()) map.put(nums2[i],-1);
+            else map.put(nums2[i], stack.peek());
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i] = map.get(nums1[i]);
         }
         return res;
     }
