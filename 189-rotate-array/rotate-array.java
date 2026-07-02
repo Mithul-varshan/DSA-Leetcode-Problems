f8fd54bd6e74a47d833c3579bb5e1d36edbec8ed
Mithul Varshan S K <mithul0605@gmail.com>
2026-07-02 16:11:29 +0530

    Time: 1 ms (40.96%) | Memory: 63.1 MB (19.95%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,7 +2,8 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         int j=0;
-        k=k%n;
+        k=k%n;    // when nums=[1,2,3,4,5] and k = 7 n-k=-2 which results in outofbound in loop so we use this 
+        // at 1st rotation it will be [5,1,2,3,4] at 5th rotation also [5,1,2,3,4] 5%5 == 1 so k=k%n we are doing
         int[] dup = new int[n];
         for(int i=n-k;i<n;i++){
             dup[j++] = nums[i];
