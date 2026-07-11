e33a8918e8090ab92b5b86d2b47356d57f000190
Mithul Varshan S K <mithul0605@gmail.com>
2026-07-11 20:40:48 +0530

    Time: 5 ms (86.67%) | Memory: 45.5 MB (73.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,51 +1,16 @@
-import java.util.Map;
-import java.util.HashMap;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // int start = 0;
-        // int maxLen = 0;
-        // Map<Character,Integer> seen = new HashMap<>();
-        // for(int end =0;end<s.length();end++){
-        //     char ch = s.charAt(end);
-        //     seen.put(ch,seen.getOrDefault(ch,0)+1);
-
-        //     while(seen.get(ch) > 1){
-        //         char startChar = s.charAt(start);
-        //         seen.put(startChar,seen.get(startChar)-1);
-        //         start++;
-        //     }
-        //     maxLen = Math.max(maxLen,end - start + 1);
-        // }
-        // return maxLen;
-        
-        //BRUTE FORCE
-        // int maxLen =0;
-        // for(int i=0;i<s.length();i++){
-        //     int[] n = new int[256] ;
-        //     int len =0;
-        //     for(int j=i;j<s.length();j++){
-        //         if(n[s.charAt(j)] == 1) break;
-        //         len = j-i+1;
-        //         maxLen = Math.max(maxLen,len);
-        //         n[s.charAt(j)] = 1;
-        //     }
-        // }
-        // return maxLen;    
-
-        //Sliding window
-
-        HashMap<Character,Integer> map = new HashMap<>();
-        int left = 0;
-        int n = s.length();
-        int maxLen =0;
-        for(int right=0;right<n;right++){
-            char ch = s.charAt(right);
-            if(map.containsKey(ch) && map.get(ch) >= left){
-                left = map.get(ch) + 1;
+        int l =0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max =0;
+        for(int r=0;r<s.length();r++){
+            char ch = s.charAt(r);
+            if(map.containsKey(ch) && map.get(ch) >= l){
+                l = map.get(ch)+1;
             }
-            map.put(ch,right);
-            maxLen = Math.max(maxLen,right-left+1);
+            map.put(ch,r);
+            max = Math.max(max, r - l+1);
         }
-        return maxLen;
+        return max;
     }
 }
