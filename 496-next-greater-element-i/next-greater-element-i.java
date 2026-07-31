e83c1d334b442b09e189790dcf7e202e286680e2
Mithul Varshan S K <mithul0605@gmail.com>
2026-07-31 16:15:02 +0530

    Time: 8 ms (5.20%) | Memory: 44.7 MB (86.38%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,37 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // for(int i=0;i<nums1.length;i++){
-        //     int ans=-1;
-        //     for(int j=0;j<nums2.length-1;j++){ 
-        //         if(nums2[j] == nums1[i]){
-        //             for (int k=j+1;k<nums2.length;k++) {
-        //                 if (nums2[k] > nums1[i]) {
-        //                     ans = nums2[k];
-        //                     break;
-        //                 }
-        //             }
-        //             break; 
-        //         }
-        //     }
-        //     nums1[i] = ans; 
-        // }
-        // return nums1;
-
-        // Stack solution :
-        HashMap <Integer , Integer> map = new HashMap<>();
-        Stack <Integer> stack = new Stack();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty()) map.put(nums2[i],-1);
-            else map.put(nums2[i], stack.peek());
-            stack.push(nums2[i]);
-        }
-        int[] res = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            res[i] = map.get(nums1[i]);
+            int result=-1;
+            for(int j=0;j<nums2.length-1;j++){
+                if(nums2[j] == nums1[i]){
+                    for(int k=j;k<nums2.length;k++){
+                        if(nums2[k] > nums1[i]){
+                            result = nums2[k];
+                            break;
+                        }
+                    }
+                }
+            }
+            nums1[i] = result;
         }
-        return res;
+        return nums1;
     }
 }
