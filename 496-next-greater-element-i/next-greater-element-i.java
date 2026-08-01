a13c03fab1c01b7c7a718a2737aa634f52cf8736
Mithul Varshan S K <mithul0605@gmail.com>
2026-08-01 11:49:54 +0530

    Time: 5 ms (15.20%) | Memory: 44.7 MB (91.00%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,19 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // for(int i=0;i<nums1.length;i++){
+        //     int result=-1;
+        //     for(int j=0;j<nums2.length-1;j++){
+        //         if(nums2[j] == nums1[i]){
+        //             for(int k=j;k<nums2.length;k++){
+        //                 if(nums2[k] > nums1[i]){
+        //                     result = nums2[k];
+        //                     break;
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     nums1[i] = result;
+        // }
+        // return nums1;
+        int[] arr = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            int result=-1;
-            for(int j=0;j<nums2.length-1;j++){
-                if(nums2[j] == nums1[i]){
-                    for(int k=j;k<nums2.length;k++){
-                        if(nums2[k] > nums1[i]){
-                            result = nums2[k];
-                            break;
-                        }
-                    }
+            arr[i] = -1;
+            boolean found = false;
+            for(int j=0;j<nums2.length;j++){
+                if(nums2[j] == nums1[i] ){
+                    found = true;
+                    continue;
+                }
+                if(found && nums2[j] > nums1[i]){
+                    arr[i] = nums2[j];
+                    break;
                 }
             }
-            nums1[i] = result;
         }
-        return nums1;
+        return arr;
     }
 }
