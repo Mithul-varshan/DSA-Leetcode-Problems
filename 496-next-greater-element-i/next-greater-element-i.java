bd8316fd3b93789474fff97f2b17ce698f9444a6
Mithul Varshan S K <mithul0605@gmail.com>
2026-08-01 16:21:06 +0530

    Time: 4 ms (73.89%) | Memory: 45.4 MB (22.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -15,21 +15,40 @@ class Solution {
         //     nums1[i] = result;
         // }
         // return nums1;
-        int[] arr = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            arr[i] = -1;
-            boolean found = false;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j] == nums1[i] ){
-                    found = true;
-                    continue;
-                }
-                if(found && nums2[j] > nums1[i]){
-                    arr[i] = nums2[j];
-                    break;
-                }
+
+        //better solution
+        // int[] arr = new int[nums1.length];
+        // for(int i=0;i<nums1.length;i++){
+        //     arr[i] = -1;
+        //     boolean found = false;
+        //     for(int j=0;j<nums2.length;j++){
+        //         if(nums2[j] == nums1[i] ){
+        //             found = true;
+        //             continue;
+        //         }
+        //         if(found && nums2[j] > nums1[i]){
+        //             arr[i] = nums2[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return arr;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
+                stack.pop();
             }
+            if(stack.isEmpty()) map.put(nums2[i], -1);
+            else map.put(nums2[i], stack.peek());
+            stack.push(nums2[i]);
+
         }
-        return arr;
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
     }
 }
