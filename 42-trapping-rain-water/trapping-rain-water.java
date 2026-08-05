2c237f93d75038d3448ead4281b2ab5062bf80e9
Mithul Varshan S K <mithul0605@gmail.com>
2026-08-05 17:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (92.02%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,47 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        // int lgh=0;
-        // int rgh=0;
-        // int res=0;
-        // int n = height.length;
-        // for(int i=0;i<n;i++){
-
-        //     lgh = height[i];
-        //     for(int j=i-1;j>=0;j--){
-        //         if(height[j] > lgh) lgh = height[j];
-        //     }
-            
-        //     rgh = height[i];
-        //     for(int j=i+1;j<n;j++){
-        //         if(height[j] > rgh) rgh = height[j];
-        //     }
-
-        //     res = res + Math.min(lgh,rgh) - height[i];
-        // }
-        // return res;
-
-        int n = height.length;
-        if (n == 0) return 0;
-
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
-
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        // Two pointers
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+        while(left < right) {
+            if(height[left] < height[right]){
+                if(height[left] > leftMax){
+                    leftMax = height[left];
+                }
+                else water+=leftMax - height[left];
+                left++;
+            }
+            else {
+                if(height[right] > rightMax){
+                    rightMax = height[right];
+                }
+                else water+=rightMax - height[right];
+                right--;
+            }
         }
-
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
-
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            res += Math.min(leftMax[i], rightMax[i]) - height[i];
-        }
-        return res;
-
-        //next should be done in two pointers
+        return water;
     }
 }
