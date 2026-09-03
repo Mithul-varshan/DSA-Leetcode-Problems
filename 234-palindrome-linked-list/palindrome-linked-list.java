7fc02f2fc2fbe57fd443c338e468081ae3f28a4e
Mithul Varshan S K <mithul0605@gmail.com>
2026-09-03 18:30:28 +0530

    Time: 3 ms (99.80%) | Memory: 94.5 MB (54.72%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,16 +10,42 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode th = head;
-        StringBuilder sb = new StringBuilder();
-        while(th != null){
-            sb.append(th.val);
-            th = th.next;
+        // ListNode current = head;
+        // ListNode prev = null;
+        // while(current != null){
+        //     ListNode temp = current;
+        //     current = current.next;
+        //     temp.next = prev;
+        //     prev = temp;
+        // }
+        // while(head != null && prev != null){
+        //     if(head.val != prev.val) return false;
+        //     head = head.next;
+        //     prev = prev.next;
+        // }
+        // return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        String original = sb.toString();
-        String reversed = new StringBuilder(original).reverse().toString();
-        return original.equals(reversed);
 
-
+        ListNode  prev = null;
+        ListNode curr = slow;
+        while(curr != null) {
+            ListNode temp = curr;
+            curr = curr.next;
+            temp.next = prev;
+            prev = temp;
+        }
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while(p2 != null){
+            if(p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
     }
 }
